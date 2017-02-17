471029617de83193a4be5ddb400c1ded844c7df9
Trevor Jorgenson <trevor@sn9.org>
2017-02-16 19:56:05 -0800

    Create iitc-plugin-inactive-ages.meta.js

---
+++ inactive-ages/iitc-plugin-inactive-ages.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @id             iitc-plugin-portal-ages@nerd4lyfe
+// @name           IITC plugin: Portal Inactive Ages
+// @category       Layer
+// @version        0.1.0.20170216.01
+// @namespace      https://github.com/seattletechie/iitc-plugins
+// @updateURL      https://github.com/seattletechie/iitc-plugins/raw/master/inactive-ages/iitc-plugin-inactive-ages.meta.js
+// @downloadURL    https://github.com/seattletechie/iitc-plugins/raw/master/inactive-ages/iitc-plugin-inactive-ages.user.js
+// @description    [iitc-2017-02-16-01] Show portal ages on the map.
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
